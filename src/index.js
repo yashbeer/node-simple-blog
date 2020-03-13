27079dcee798ae2813360a602b4b36b175d0ceda
Ubuntu <ubuntu@ip-172-31-40-17.ap-south-1.compute.internal>
2020-03-13 21:02:56 +0000

    Index file

--- src/index.js
+++ src/index.js
@@ -6,5 +6,16 @@ app.listen(port, () => {
 })
 
 app.get('', (req, res) => {
-    res.send('<h1>Hello World</h1><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>')
+    var str = '<img src="https://frapp.in/static/media/frapp-logo.ad585c09.svg" width="110" />'
+         str += '   <center><p>A Simple Blog Using Node</p></center>'
+         str += '<p style="display: inline-block; position: absolute; right: 55px; top: 10px;"><a href="/api/users/me" target="_blank">User Profile</a></p>'
+         str += '   <hr>'
+         str += '   <center>'
+         str += '     <p></p>'
+         str += '     <p><a href="/api/posts" target="_blank">Post List</a></p>'
+         str += '     <p><a href="/api/posts/5e6b795bd01e17229fb7eaea" target="_blank">Post Details</a></p>'
+         str += '   </center>'
+         str += '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>'
+         
+    res.send(str)
 })
